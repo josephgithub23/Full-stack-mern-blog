274cc7aba6dae9e4ae27eb4bc1004a0c4ada34f1
Joseph Tobi Olatilo <125037114+josephgithub23@users.noreply.github.com>
2024-07-23 16:17:18 +0100

    create and start the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -39,7 +39,9 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to={"/sign-in"}>
-          <Button gradientDuoTone={"purpleToBlue"}>Sign In</Button>
+          <Button gradientDuoTone={"purpleToBlue"} outline>
+            Sign In
+          </Button>
         </Link>
         <Navbar.Toggle className="" />
       </div>
