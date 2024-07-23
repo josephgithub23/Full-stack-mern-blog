c638c3f87e00930406b6b1d039a8822b70a9adc6
Joseph Tobi Olatilo <125037114+josephgithub23@users.noreply.github.com>
2024-07-23 17:20:10 +0100

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
