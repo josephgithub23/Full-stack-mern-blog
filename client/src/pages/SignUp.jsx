8987f6f6823d23055f51630a04501381946a2a42
Joseph Tobi Olatilo <125037114+josephgithub23@users.noreply.github.com>
2024-07-24 13:16:10 +0100

    add functionality to the signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,55 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value.trim(),
+    }); /**trim is to remove the white space from the input fields in case a user press space bar */
+  };
+
+  /**
+   * Since we are making request to the backend port 3000, then we need to change the proxy from vite config file
+   *
+   */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please all fields are required");
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(false);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sing-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className=" flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -10,7 +57,7 @@ export default function SignUp() {
         <div className=" flex-1">
           <Link to={"/"} className="font-bold   dark:text-white text-4xl">
             <span className="px-2 p-y-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              TobildCode{" "}
+              TobildCode
             </span>
             Blog
           </Link>
@@ -21,36 +68,63 @@ export default function SignUp() {
         </div>
         {/* right */}
         <div className=" flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your Email" />
               <TextInput
-                type="text"
+                type="email"
                 placeholder="name@company.com"
                 id="email"
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value="Your Password" />
-              <TextInput type="password" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone={"purpleToPink"} type="submit">
-              Sign Up
+            <Button
+              gradientDuoTone={"purpleToPink"}
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  (<Spinner size={"sm"} />
+                  <span className="p-3">Loading...</span>)
+                </>
+              ) : (
+                " Sign Up"
+              )}
             </Button>
           </form>
           <div className="flex mt-4 text-sm gap-2">
             <span>Have an account? </span>
             <Link to={"/sign-in"} className="text-blue-500">
-              {" "}
               Sign In
             </Link>
           </div>
+          <div className="">
+            {errorMessage && (
+              <Alert className="mt-4" color={"failure"}>
+                {errorMessage}
+              </Alert>
+            )}
+          </div>
         </div>
-        <div className=""></div>
       </div>
     </div>
   );
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+/*
+ * We change the proxy to 3000
+ * from the front end. we also set secure: false. because the port request is coming through http not https.
+ */
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api/": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
